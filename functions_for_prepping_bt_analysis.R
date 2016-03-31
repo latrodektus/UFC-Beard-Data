d7c321d63964ea6141f0965e07a2417884208642
wcornwell <wcornwell@gmail.com>
2016-03-31 18:05:31 +1100

    bug fix and a few improvements

--- functions_for_prepping_bt_analysis.R
+++ functions_for_prepping_bt_analysis.R
@@ -1,8 +1,7 @@
 
 
 set_up_btm<-function(predictors,winner,loser){
-  winner$ID<-as.factor(winner$ID)
-  loser$ID<-as.factor(loser$ID)
+  
   names(predictors)[1]<-"ID"
   predictors<-arrange(predictors,ID)
   row.names(predictors)<-predictors$ID
@@ -16,6 +15,24 @@ set_up_btm<-function(predictors,winner,loser){
   beards<-list(winner=winner2,loser=loser2,predictors=predictors)
   return(beards)
 }
+  
+  
+#   names(predictors)[1]<-"ID"
+#   predictors<-arrange(predictors,ID)
+#   row.names(predictors)<-predictors$ID
+#   names(winner)[1]<-"ID"
+#   names(loser)[1]<-"ID"
+#   winner$ID<-as.factor(winner$ID)
+#   loser$ID<-as.factor(loser$ID)
+#   
+#   all<-rbind(winner,loser)
+#   all$winning<-c(rep("yes",length(winner$ID)),rep("no",length(winner$ID)))
+#   all$ID<-as.factor(all$ID)
+#   winner2<-filter(all,winning=="yes")
+#   loser2<-filter(all,winning=="no")
+#   beards<-list(winner=winner2,loser=loser2,predictors=predictors)
+#   return(beards)
+# }
 
 get_bt_abilities<-function(model,predictors=predictors){
   nm <- grep("[ID]", names(coef(model)),fixed = TRUE, value = TRUE)
